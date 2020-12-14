689a9399102fc5c7b09292111546de4bd3851a55
velkovska <velkovskasaska@gmail.com>
2020-12-14 14:53:44 +0100

    adding port

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/profile/:id', (req, res) => { msWriteProfilerMark.handleProfileGet(req
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, ()=> {
-	console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, ()=> {
+	console.log(`app is running on port 3001 ${process.env.PORT}`);
 })
