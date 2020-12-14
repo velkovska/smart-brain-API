aa6c4dfa5bfeccd710d34eebde33f362a4a70392
velkovska <velkovskasaska@gmail.com>
2020-12-14 15:06:24 +0100

    minor change

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3001, ()=> {
-	console.log(`app is running on port 3001 ${process.env.PORT}`);
+	console.log(`app is running on port ${process.env.PORT}`);
 })
