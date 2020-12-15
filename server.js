a6a3feac5053fc48593824a7523a5dc7ca62b915
velkovska <velkovskasaska@gmail.com>
2020-12-15 16:51:21 +0100

    test

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-	  connectionString : process.env.DATABASE_URL,
-	  ssl: true
+	  host : '127.0.0.1',
+	  user : 'postgres',
+	  password : 'test',
+	  database : 'smart-brain'
 	}
-}); 
+});
 
 const app = express();
 
